564d2f716ad565a9fa9071b3a41a75109f8af0b7
Efio-esien Efiom <efio-esien@tangerine.africa>
2026-06-01 18:18:22 +0100

    added pom for login and add to cart and created some fixtures

---
+++ pages/add_to_cart.py
@@ -0,0 +1,27 @@
+import re 
+from playwright.sync_api import Page, expect
+
+class Add_to_cart:
+
+    def __init__(self, page : Page):
+
+        self.page = page
+
+        #Locators
+        self.add_to_cart = page.locator("[data-test=\"add-to-cart\"]")
+        self.view_item_locator = page.locator("[data-test=\"item-1-title-link\"]")
+        self.view_item_in_cart = page.locator("[data-test=\"item-quantity\"]")
+
+    #Methods
+    def is_add_to_cart_visible(self):
+        return self.add_to_cart.is_visible()
+    
+    def check_item_in_cart(self):
+        item_count = self.view_item_locator.count
+        assert item_count
+    
+    def view_item_in_inventory(self):
+        self.view_item_locator.click()
+
+    def click_add_to_cart_button(self):
+        self.add_to_cart.click()
--- tests/test_login_test.py
+++ tests/test_login_test.py
@@ -1,16 +1,46 @@
-from playwright.sync_api import sync_playwright
+#from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page, expect
+from pages.login_page import LoginPage
+import os
+from dotenv import load_dotenv
 
-def run():
-    with sync_playwright() as p:
+
+load_dotenv(override=False)
+
+def test_login (page : Page) -> None:
+
+    base_url = os.getenv("BASE_URL")
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+
     
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto('https://www.saucedemo.com')
-        page.fill('input[name="user-name"]', 'standard_user')
-        page.fill('input[name="password"]', 'secret_sauce')
-        page.locator('#login-button').click()
-        print("Login successful!")
-        browser.close()
+    
 
-if __name__ == "__main__":
-    run()
+    #load_dotenv(override=False)
+
+    #my_username = os.getenv("USERNAME")
+    # login_page.load_page()
+    page.goto(base_url)
+
+    login_page = LoginPage(page)
+    login_page.enter_username(username)
+    login_page.enter_password(password)
+    login_page.click_login()
+
+
+
+
+# def run():
+#     with sync_playwright() as p:
+    
+#         browser = p.chromium.launch(headless=False)
+#         page = browser.new_page()
+#         page.goto('https://www.saucedemo.com')
+#         page.fill('input[name="user-name"]', 'standard_user')
+#         page.fill('input[name="password"]', 'secret_sauce')
+#         page.locator('#login-button').click()
+#         print("Login successful!")
+#         browser.close()
+
+# if __name__ == "__main__":
+#     run()
